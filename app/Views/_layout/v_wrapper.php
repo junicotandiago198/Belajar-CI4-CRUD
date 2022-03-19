579dc3c8772ad2f2808fb9e2c46c5f0389896f71
Junico Tandiago <junicotandiagoo21@gmail.com>
2022-03-19 20:58:12 +0700

    Membuat wrapper dan echo semua layout

---
+++ app/Views/_layout/v_wrapper.php
@@ -0,0 +1,6 @@
+<?php echo view('_layout/v_header.php'); ?>
+<?php echo view('_layout/v_navbar.php'); ?>
+<?php echo view('_layout/v_sidebar.php'); ?>
+<?php echo view('_layout/v_content.php'); ?>
+<?php echo view('_layout/v_footer.php'); ?>
+<?php echo view('_layout/v_js.php'); ?>
