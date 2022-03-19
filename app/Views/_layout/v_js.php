427e204fbae7608c800712b2eb3afc6fcc28e213
Junico Tandiago <junicotandiagoo21@gmail.com>
2022-03-19 20:57:53 +0700

    Pemisahaan tamplate

---
+++ app/Views/_layout/v_js.php
@@ -0,0 +1,11 @@
+
+<!-- jQuery -->
+<script src=".<?= base_url() ?>/assets/plugins/jquery/jquery.min.js"></script>
+<!-- Bootstrap 4 -->
+<script src=".<?= base_url() ?>/assets/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- AdminLTE App -->
+<script src="<?= base_url(); ?>/assets/dist/js/adminlte.min.js"></script>
+<!-- AdminLTE for demo purposes -->
+<script src="<?= base_url(); ?>/assets/dist/js/demo.js"></script>
+</body>
+</html>
