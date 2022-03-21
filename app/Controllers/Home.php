b5d8198301bf754623ef6463866794903e6ad4ec
Junico Tandiago <junicotandiagoo21@gmail.com>
2022-03-21 12:55:40 +0700

    Penambahan function construct dan pengambilan data melalui TestimonialModel

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -2,12 +2,22 @@
 
 namespace App\Controllers;
 
+use CodeIgniter\Controller;
+use App\Models\TestimonialModel;
+
 class Home extends BaseController
 {
+
+    public function __construct()
+    {
+        $this->Testimonial = new TestimonialModel();
+    }
+
     public function index()
     {
         $data = [
-            'title'     => 'Dashboard',
+            'title'         => 'Dashboard',
+            'testimonial'   => $this->Testimonial->getTestimonial()
         ];
         return view('welcome_message', $data);
     }
