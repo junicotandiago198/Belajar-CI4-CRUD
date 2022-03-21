bc8aa301d2016b0f3c5e312faa93eb42bfb8725d
Junico Tandiago <junicotandiagoo21@gmail.com>
2022-03-21 13:01:18 +0700

    penambahan perulangan data untuk read data yang sudah di create

--- app/Views/admin/testimonial/index.php
+++ app/Views/admin/testimonial/index.php
@@ -1,51 +1,63 @@
-<!-- Content Wrapper. Contains page content -->
 <div class="content-wrapper">
 <!-- Content Header (Page header) -->
 <section class="content-header">
-	<div class="container-fluid">
-	<div class="row mb-2">
-		<div class="col-sm-6">
-		<h1>Blank Page</h1>
-		</div>
-		<div class="col-sm-6">
-		<ol class="breadcrumb float-sm-right">
-			<li class="breadcrumb-item"><a href="#">Home</a></li>
-			<li class="breadcrumb-item active">Blank Page</li>
-		</ol>
-		</div>
-	</div>
-	</div><!-- /.container-fluid -->
+<div class="container-fluid">
+<div class="row mb-2">
+<div class="col-sm-6">
+<h1>Testimonial</h1>
+</div>
+<div class="col-sm-6">
+<ol class="breadcrumb float-sm-right">
+	<li class="breadcrumb-item"><a href="#">Home</a></li>
+	<li class="breadcrumb-item active">Testimonial</li>
+</ol>
+</div>
+</div>
+</div><!-- /.container-fluid -->
 </section>
 
 <!-- Main content -->
 <section class="content">
-
-	<!-- Default box -->
-	<div class="card">
+<div class="container-fluid">
+<div class="row">
+<div class="col-12">
+<div class="card">
 	<div class="card-header">
-		<h3 class="card-title">Title</h3>
-
-		<div class="card-tools">
-		<button type="button" class="btn btn-tool" data-card-widget="collapse" title="Collapse">
-			<i class="fas fa-minus"></i>
-		</button>
-		<button type="button" class="btn btn-tool" data-card-widget="remove" title="Remove">
-			<i class="fas fa-times"></i>
-		</button>
-		</div>
-	</div>
+	<a class="btn btn-success" href="<?= base_url(); ?>/admin/testimonial/create">Tambah</a>
+	</div> 
+	<!-- /.card-header -->
 	<div class="card-body">
-		Start creating your amazing application!
+	<table id="example2" class="table table-bordered table-hover">
+		<thead>
+		<tr>
+			<th>No</th>
+			<th>Username</th>
+			<th>Deskripsi</th>
+			<th>Image</th>
+		</tr>
+		</thead>
+		<tbody>
+		<?php $nomor = 0; ?>
+		<?php foreach($testimonial as $key => $row) { ?>
+		<tr>
+			<td><?= ++$nomor; ?></td>
+			<td><?= $row['username'];  ?></td>
+			<td><?= $row['deskripsi']; ?></td>
+			<td><img class="rounded-circle" width='50' height='50' src="<?= base_url('uploads/' .$row['image']); ?>" alt="Image"></td>
+		</tr>
+		<?php } ?>
+		</tbody>
+	</table>
 	</div>
 	<!-- /.card-body -->
-	<div class="card-footer">
-		Footer
-	</div>
-	<!-- /.card-footer-->
-	</div>
-	<!-- /.card -->
-
+</div>
+<!-- /.card -->
+</div>
+<!-- /.col -->
+</div>
+<!-- /.row -->
+</div>
+<!-- /.container-fluid -->
 </section>
 <!-- /.content -->
 </div>
-<!-- /.content-wrapper -->
