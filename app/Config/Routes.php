552bbf61b792b604bf0760e9f6e475a019b60e2a
Junico Tandiago <junicotandiagoo21@gmail.com>
2022-03-21 12:53:45 +0700

    Penambahan Routing untuk admin home, admin testimonial create dan store

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -32,7 +32,10 @@ $routes->setAutoRoute(true);
 // We get a performance increase by specifying the default
 // route since we don't have to scan directories.
 $routes->get('/', 'Home::index');
+$routes->get('/admin/home', 'Admin::home');
 $routes->get('/admin/testimonial', 'Admin::index');
+$routes->get('/admin/testimonial/create', 'Admin::create');
+$routes->post('/admin/testimonial/store', 'Admin::store');
 
 
 /*
